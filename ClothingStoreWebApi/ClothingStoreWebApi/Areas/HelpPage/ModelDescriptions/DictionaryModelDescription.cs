030279119bc9f91545198b4e7ea491e025acdeb6
nuwanwixx <nuwan.wixx@gmail.com>
2018-05-06 18:26:55 +0530

    First Commit

---
+++ ClothingStoreWebApi/ClothingStoreWebApi/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace ClothingStoreWebApi.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ ClothingStoreWebApi/ClothingStoreWebApi/Areas/HelpPage/ModelDescriptions/IModelDocumentationProvider.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Reflection;
+
+namespace ClothingStoreWebApi.Areas.HelpPage.ModelDescriptions
+{
+    public interface IModelDocumentationProvider
+    {
+        string GetDocumentation(MemberInfo member);
+
+        string GetDocumentation(Type type);
+    }
+}
