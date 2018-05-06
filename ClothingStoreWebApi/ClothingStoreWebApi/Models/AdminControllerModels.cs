31b0a2c338b081e0aace1a4a4dd4cbcd86949555
nuwanwixx <nuwan.wixx@gmail.com>
2018-05-06 19:04:35 +0530

    enebled-migration / initial migration/ add AdminModel Class

---
+++ ClothingStoreWebApi/ClothingStoreWebApi/Models/AdminControllerModels.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ClothingStoreWebApi.Models
+{
+    public class AdminControllerModels
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        
+    }
+}
